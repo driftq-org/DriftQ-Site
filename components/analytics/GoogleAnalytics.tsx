845a34d9a6c30f8b50cd9bfc9e12095d8f92be9a
Behnam Amiri <behamiridev@gmail.com>
2026-02-14 09:17:09 -0700

    :)

--- components/analytics/GoogleAnalytics.tsx
+++ components/analytics/GoogleAnalytics.tsx
@@ -12,14 +12,15 @@ export default function GoogleAnalytics() {
   return (
     <>
       <Script
+        async
         src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
         strategy="afterInteractive"
       />
+
       <Script id="ga4-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
-          window.gtag = gtag;
           gtag('js', new Date());
           gtag('config', '${measurementId}', { send_page_view: false });
         `}
